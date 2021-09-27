e84773ba79190ccc581ca83be90e6041760057f8
RosalRicardo <crosalri@jci.com>
2021-09-27 08:28:29 -0300

    documentação weave efeito pelicular

---
+++ automato celulares/jogoDaVida.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
+#include <errno.h>
+
+#define TAM 100
+
+int fita[TAM][TAM];
+int temp[TAM][TAM];
+
+int msleep(long msec);
+
+int main(){
+    return 0;
+}
