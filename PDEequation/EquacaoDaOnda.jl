ffb36790042adb4700068ce4fa0783b47d61ee8b
RosalRicardo <ricardo.rosal@icloud.com>
2021-10-17 19:46:28 -0300

    cap1 intro eqdiff julia

---
+++ PDEequation/EquacaoDaOnda.jl
@@ -0,0 +1,6 @@
+d = ChebyshevInterval()^2                   # Defines a rectangle
+Δ = Laplacian(d)                            # Represent the Laplacian
+f = ones(∂(d))                              # one at the boundary
+u = \([Dirichlet(d); Δ+100I], [f;0.];       # Solve the PDE
+                tolerance=1E-5)             
+surface(u)                                  # Surface plot  A
